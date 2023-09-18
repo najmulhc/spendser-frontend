3813e131749e0c2e439123a6ce698efe7d72782f
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-09-18 21:47:36 +0600

    user can see their balence

--- src/app/hooks/useUser.ts
+++ src/app/hooks/useUser.ts
@@ -4,16 +4,20 @@ import getUser from "../services/getUser";
 import { User } from "@/types";
 import { useSelector } from "react-redux";
 import { StoreType } from "../redux/store";
+import { useDispatch } from "react-redux";
+import { setAccount } from "../redux/features/accountSlice";
 
 const useUser = (token:string) => {
   const [stateUser, setStateUser] = useState<User>({ username: "", email: "" });
 
   const { user } = useSelector((state: StoreType) => state);
+  const dipatch = useDispatch()
  
 
   useEffect(() => {
     if (!user.email) {
       getUser(token).then((data) => {
+        dipatch(setAccount(data.user.account))
         setStateUser({
           username: data.user?.username,
           email: data.user?.email,
