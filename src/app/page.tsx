b20450c15e9b2b90b3b18d1bf9130cb6f34c49fc
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-09-18 13:23:49 +0600

    home fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-"use client";
 
-import DashboardHeader from "./dashboard/components/DashboardHeader";
-import useUser from "./hooks/useUser";
+
+import DashboardHeader from "./dashboard/components/DashboardHeader"; 
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  const token = (localStorage?.getItem("token") as string) || "";
-  const { user } = useUser(token);
+ 
 
   return (
     <>
       <DashboardHeader />
       <main>
-        <h1 className="text-2xl font-semibold"> {user?.username}</h1>
+        <h1 className="text-2xl font-semibold"> welcome to my world!</h1>
       </main>
     </>
   );
