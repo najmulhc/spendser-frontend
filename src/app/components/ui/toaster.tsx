0268dbdb77a240bc1371ebbfb27278b2caf8454b
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-10-18 17:59:29 +0600

    another version added

--- src/app/components/ui/toaster.tsx
+++ src/app/components/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "src/app/components/ui/toast"
-import { useToast } from "src/app/components/ui/use-toast"
+} from "./toast"
+import { useToast } from "./use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
